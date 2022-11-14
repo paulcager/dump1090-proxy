00a0c398285723f93457c69b7e1bee9a33358074
Paul Cager <paul.cager@gmail.com>
2022-11-14 13:38:17 +0000

    Rotate DB files

--- cmd/dump1090-logger/main.go
+++ cmd/dump1090-logger/main.go
@@ -35,32 +35,10 @@ func main() {
 
 	logger = log.NewLogfmtLogger(os.Stderr)
 
-	db, err := createDb()
-	if err != nil {
-		panic(err)
-	}
-
 	ch := make(chan sbs.Message, 32)
 
-	go dbWriter(db, ch)
+	go dbWriter(ch)
 	consume(*address, ch)
-}
-
-func createDb() (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", "./dump1090.db")
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = db.Exec(`
-			create table messages(type integer, timestamp text, hexIdent text, lat float64, lon float64, alt float64)
-		`)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
 
 func consume(addr *net.TCPAddr, ch chan sbs.Message) {
@@ -123,27 +101,81 @@ func runConnection(conn *net.TCPConn, ch chan<- sbs.Message) {
 	}
 }
 
-func dbWriter(db *sql.DB, ch <-chan sbs.Message) {
+func dbWriter(ch <-chan sbs.Message) {
 	count := 0
-	stmt, err := db.Prepare(`
-			insert into messages(type, timestamp , hexIdent, lat, lon, alt)
-			values(?, strftime('%Y-%m-%d %H:%M:%f', ?), ?, ?, ?, ?)`)
-	if err != nil {
-		panic(err)
-	}
-	defer stmt.Close()
 
+	defer closeDb()
 	for m := range ch {
-		if !math.IsNaN(m.Latitude) && !math.IsNaN(m.Longitude) {
-			count++
-			if count%100 == 0 {
-				fmt.Println(count, int(m.Type), m.Timestamp, m.HexIdent, m.Latitude, m.Longitude, m.Altitude)
-			}
-			_, err := stmt.Exec(int(m.Type), m.Timestamp, m.HexIdent, m.Latitude, m.Longitude, m.Altitude)
-			if err != nil {
-				// Almost certainly unrecoverable.
-				panic(err)
-			}
+		if math.IsNaN(m.Latitude) || math.IsNaN(m.Longitude) {
+			continue
+		}
+
+		count++
+		if count%1000 == 0 {
+			fmt.Println(count, int(m.Type), m.Timestamp, m.HexIdent, m.Latitude, m.Longitude, m.Altitude)
+		}
+
+		err := writeToDb(m)
+		if err != nil {
+			// Almost certainly unrecoverable.
+			panic(err)
 		}
 	}
+}
+
+var (
+	db         *sql.DB
+	stmt       *sql.Stmt
+	nextRotate time.Time
+)
+
+func writeToDb(m sbs.Message) error {
+	if nextRotate.IsZero() || m.Timestamp.After(nextRotate) {
+		// Need to rotate DB
+		closeDb()
+
+		nextRotate = m.Timestamp.Truncate(24 * time.Hour).Add(24 * time.Hour)
+		fileName := "./dump1090-" + m.Timestamp.Format("2006-01-02") + ".db"
+		level.Info(logger).Log("rotating", fileName)
+		if err := createDb(fileName); err != nil {
+			return err
+		}
+	}
+
+	_, err := stmt.Exec(int(m.Type), m.Timestamp, m.HexIdent, m.Latitude, m.Longitude, m.Altitude)
+	return err
+}
+
+func closeDb() {
+	if stmt != nil {
+		stmt.Close()
+	}
+	if db != nil {
+		db.Close()
+	}
+
+	stmt = nil
+	db = nil
+}
+
+func createDb(name string) error {
+	var err error
+	db, err = sql.Open("sqlite3", name)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`
+			create table messages(type integer, timestamp text, hexIdent text, lat float64, lon float64, alt float64)
+		`)
+
+	if err != nil {
+		return err
+	}
+
+	stmt, err = db.Prepare(`
+			insert into messages(type, timestamp , hexIdent, lat, lon, alt)
+			values(?, strftime('%Y-%m-%d %H:%M:%f', ?), ?, ?, ?, ?)`)
+
+	return err
 }
